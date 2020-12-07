43d7d364764277222f40aaf695360a42cc0cc218
Aditya Gupta <adityag.ug19.cs@nitp.ac.in>
2020-12-07 13:10:39 +0530

    Initiate... from graphMat
    Merge with upstream
    
    3D_Graph_Matrix
    
    BookKeeping_Commit: Coming back to it 20 Dec
    
    Added templated Matrix::Init that accepts data reference
    
    Added the verification code as submodule
    
    Auto Expanding Ability Added; Callback feature in dev
    
    Added experimental memory_pool;Callback based init
    
    Behaviour of all add_*_layer almost exactly same now, for better understanding;
    Removed some cluttered comments
    
    Details:
    Modified pause_expansion() behaviour to cope with infinite blocking of main thread
    Added get_size
    set_expansion_rate() added for simpler access to modify the rate (not much change to logic, just abstracted it)
    Added initiliaser based constructor (this also resize already provided logic)
    MOST NOTICIABLE IS - Completed Swastic_Find
    
    Documentation for Graph_Matrix_3D
    
    Reorganised files; Added more READMEs
    
    to customization and 2D GraphMat
    
    Added NeighbourIterator; Naming Changes for util::coord
    
    Update README.md
    
    Moved headers to include/graphMat
    
    Linear and Directional iterators added
    
    Bug Fix: expand_n_unit calling resize with MANUAL resize type
    
    Updated behaviour of NeighbourIterator
    
    Earlier it used to look out the frontwards layer (+z axis) first, then go backwards. This behaviour will eventually increase the chance for being at max +z, and decreased at -z
    
    Updated template SFINAE conditions
    
    Using bool instead convar in auto expansion
    
    Minor changes in coordinates, & directionalPath
    
    Naming changes; Doxyfile added
    
    Add PKGBUILD, Updated include path
    
    Fix pkgname
    
    Add CMakeLists for use with CMake based projs
    
    Initiate...

---
+++ include/graphMat/box_base.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+/**
+ * This header is not meant to be included directly
+*/
+
+#include "direction.hpp"
+
+class Box_Base {
+    // Edit: 26th Jan, modifying these from virtual to non-virtual methods
+    //Box_Base* get_adj_box(Direction dir) const;
+
+protected:
+    Box_Base() {}
+    ~Box_Base() {}
+};
---
+++ include/graphMat/graph_box.hpp
@@ -0,0 +1,111 @@
+#pragma once
+
+#include "box_base.hpp"
+#include "../../util/coord.hpp"
+
+template<typename dtype>
+class Graph_Box_3D : Box_Base{
+public:
+    typedef int32_t dimen_t;
+
+    // @learnt @self - no need to specify templates here,since compiler can deduce from return statement (if doing definitions directly inside class)
+    /*consteval*/ inline Graph_Box_3D* get_adj_box(Direction dir) const {
+        switch (dir)
+        {
+        case Direction::UTTAR:
+            return this->UP;
+        case Direction::PURVA:
+            return this->RIGHT;
+        case Direction::PASHCHIM:
+            return this->LEFT;
+        case Direction::DAKSHIN:
+            return this->DOWN;
+        case Direction::ISHANYA:
+            return (!this->UP) ? nullptr : this->UP->RIGHT;
+        case Direction::AGNEYA:
+            return (!this->DOWN) ? nullptr : this->DOWN->RIGHT;
+        case Direction::NAIRUTYA:
+            return (!this->DOWN) ? nullptr : this->DOWN->LEFT;
+        case Direction::VAYAVYA:
+            return (!this->UP) ? nullptr : this->UP->LEFT;
+        case Direction::URDHWA:
+            return this->FRONT_FACING;
+        case Direction::ADHARASTHA:
+            return this->BACK_FACING;
+        default:
+            return nullptr;
+        }
+    }
+
+    // enable_if_t<std::is_pointer_t<std::remove_reference_t<node_dtype>>>
+    template<typename DType = dtype, typename std::enable_if<std::is_pointer_v<std::remove_reference_t<DType>>>::type >
+    Graph_Box_3D() noexcept:
+        data(nullptr) {}
+
+    Graph_Box_3D() noexcept {}
+
+#ifndef GRAPH_MAT_NO_COORD
+    Graph_Box_3D(dimen_t x, dimen_t y, dimen_t z) noexcept:
+        coordinate(x,y,z)
+        {}
+
+    const auto& get_coordinate() const noexcept {
+        return this->coordinate;
+    }
+#endif // !GRAPH_MAT_NO_COORD
+
+    template<typename DTYPE = dtype, typename = std::enable_if_t<std::is_move_constructible_v<dtype>> >
+    Graph_Box_3D(dtype&& data) :
+        data(data)
+        {}
+
+    Graph_Box_3D(const Graph_Box_3D&) = delete;
+    Graph_Box_3D(const Graph_Box_3D&&) = delete;
+
+    friend std::ostream& operator<<(std::ostream& os, const Graph_Box_3D& box) {
+        os << box.getData();
+        return os;
+    }
+
+    const dtype& getData() const { return this->data; }
+    dtype& getDataRef() { return this->data; }
+
+    Graph_Box_3D* RIGHT{ nullptr };
+    Graph_Box_3D* LEFT{ nullptr };
+    Graph_Box_3D* UP{ nullptr };
+    Graph_Box_3D* DOWN{ nullptr };
+    Graph_Box_3D* FRONT_FACING{ nullptr };
+    Graph_Box_3D* BACK_FACING{ nullptr };
+
+protected:
+#ifndef GRAPH_MAT_NO_COORD
+    util::_coord3D<dimen_t> coordinate;
+    void set_coord(dimen_t, dimen_t, dimen_t);
+    void set_coord(util::_coord3D<dimen_t>&& new_coord);
+#endif
+
+    dtype data;    /*This has been given as an extension, so that you can add more variables to the graph_box
+                        though, note that, you will be able to access using this->data->your_var_name */
+                        //        coord_type coords;  // @NOTE - not actually needed, though this maybe used in my implementation of snake
+                                // @todo - I am removing the functionality of having a coord in each graph node, in an attempt to make it as small as sufficient
+
+    // @learnt - friending a templated class (down below is the `syntax` found, to friend `all` templated versions of Graph_Matrix)
+    template<typename, typename> friend class Graph_Matrix_3D;    //will make all Graph_Matrix friend to this
+    //friend class Graph_Matrix<int, int32_t>;
+};
+
+#ifndef GRAPH_MAT_NO_COORD 
+template<typename dtype>
+inline void Graph_Box_3D<dtype>::set_coord(util::_coord3D<dimen_t>&& new_coord)
+{
+    this->coordinate = std::move(new_coord);
+}
+
+template<typename dtype>
+inline void Graph_Box_3D<dtype>::set_coord(dimen_t x, dimen_t y, dimen_t z)
+{
+    this->coordinate.mX = x;
+    this->coordinate.mY = y;
+    this->coordinate.mZ = z;
+}
+#endif // !Graph_MAT_NO_COORD
